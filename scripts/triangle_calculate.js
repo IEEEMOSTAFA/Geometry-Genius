e48b79c3d73ec18201b58a8f240074984725dff8
IEEEMOSTAFA <skmostafa8888@gmail.com>
2024-03-23 22:53:11 +0600

    traiangle card added

--- scripts/triangle_calculate.js
+++ scripts/triangle_calculate.js
@@ -1,4 +1,33 @@
 // console.log('conected with javascript');
+
+/* 
+ objective:: get base,height of a triangle .Calculate the area by using the provided formula . and then display the area.
+ step-1: get base value of the triangle
+ step -2: added an id in the base input field
+ step -3: use getElementById to access the input field
+ step-4: get value from the input field. (value is string now)
+ step -5: convert the value to a number . use ParseFloat
+*/
 function calculateTriangleArea(){
-    console.log('inside function');
+    const triangleBaseInput = document.getElementById('triangle-base');
+    const triangleBaseText = triangleBaseInput.value;
+    const base = parseFloat(triangleBaseText);
+    console.log( base);
+
+
+    // get triangle height value
+    const triangleHeightInput = document.getElementById('triangle-height');
+     
+    const triangleHeightText = triangleHeightInput.value;
+    const height = parseFloat(triangleHeightText);
+    console.log(height);
+
+
+    // calculate the area::
+    const area = 0.5 * base * height;
+    console.log('Total Area Of The Triangle is ::  ',area);
+
+    // display triangle area 
+    const triangleAreaSpan = document.getElementById('triangle-area');
+    triangleAreaSpan.innerText = area;
 }
