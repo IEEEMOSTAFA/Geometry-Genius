9e3f67521f78e833156447f4d0feddf0bc37e002
IEEEMOSTAFA <skmostafa8888@gmail.com>
2024-03-25 23:01:54 +0600

    funtion added

--- scripts/parallelogram.js
+++ scripts/parallelogram.js
@@ -39,10 +39,27 @@
 
 function CalculateParallelogram() {
     const base = getInputValueById('parallelogram-base');
+    // console.log('base value',base);
+
+    const height = getInputValueById('parallelogram-height');
+    // console.log('height value',height);
+
+    const area = base * height;
+    console.log('area of the parallelogram is ',area);
+
+    setInnerTextById('parallelogram-area', area);
 
 }
 
-function getInputValueById(inputFieldId){
+function getInputValueById(inputFieldId) {
     const inputField = document.getElementById(inputFieldId);
-    console.log(inputField.value);
+    const inputValueText = inputField.value;
+    const inputValue = parseFloat(inputValueText);
+    // console.log(inputValue);
+    return inputValue;
+}
+
+function setInnerTextById(elementId,area){
+    const element = document.getElementById(elementId);
+    element.innerText = area;
 }
